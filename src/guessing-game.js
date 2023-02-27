6e91da493fb8959198854cd1b071956aeac116d3
Kate Mihalovskaya <kate.mihalovskaya@gmail.com>
2023-02-27 22:21:40 +0100

    feat: implemented task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
+    min;
+    max;
+    currentGuess;
+    
     constructor() {}
-
+    
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.currentGuess = Math.round((this.min + this.max)/2);
+        return this.currentGuess;
     }
 
     lower() {
-
+        this.max = this.currentGuess;
     }
 
     greater() {
-
+        this.min = this.currentGuess;
     }
 }
 
